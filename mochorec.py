300c09bdc5aa5669c4ed2114c609b45eb4513dcd
Ryosuke Sato <rskjtwp@gmail.com>
2016-08-21 23:28:01 +0900

    :+1: Add command line option

--- mochorec.py
+++ mochorec.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # mochorec command line tools
 import mochorec.niconico as nico
+import argparse
 import sys
 import os
 import logging
@@ -33,17 +34,35 @@ def download(param, saveto, cmd="rtmpdump"):
     logging.debug(com)
 
 
+
+def parse():
+    parser = argparse.ArgumentParser(description="mochorec")
+    # -lv lv*********
+    parser.add_argument("-lv", help="lv is niconico video movie id",
+                        required=True, nargs=1)
+    # save path
+    parser.add_argument("--save", "-s", help="file save path and name",
+                        required=True, nargs=1)
+    # DEBUG
+    parser.add_argument("--debug", "-d", help="debug: show more messages for your debug",
+                        action='store_true')
+    return parser.parse_args()
+
+
 def main():
+    arg = parse()
     logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
-                        level=logging.DEBUG)
+                        level=logging.INFO)
+    if arg.debug is True:
+        logging.level = logging.DEBUG
     n = nico.Niconico()
     n.login()
     logging.info("[+] Login success")
 
-    status = n.getplayerstatus("lv272216525")
+    status = n.getplayerstatus(arg.lv)
     logging.info("[+] start download")
 
-    download(status, "./toramamo137.flv")
+    download(status, n.save)
 
 
 if __name__ == "__main__":
