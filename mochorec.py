b8da7bea6e06a3b993d3f5ef8a257fd047191b73
Ryosuke Sato <rskjtwp@gmail.com>
2016-10-16 17:14:56 +0900

    :+1: Add formated message

--- mochorec.py
+++ mochorec.py
@@ -31,7 +31,7 @@ def get(param, saveto, cmd="rtmpdump"):
     """get file using rtmpdump"""
     command = which(cmd)
     if not command:
-        logging.critical("[+] rtmpdump not found. Please install rtmpdump on your PATH")
+        logging.critical("✗ rtmpdump not found. Please install rtmpdump on your PATH")
         sys.exit(1)
 
     tempdir = tempfile.mkdtemp()
@@ -41,18 +41,18 @@ def get(param, saveto, cmd="rtmpdump"):
         com = "{rtmp} -r {url} -y mp4:/{content} -C S:{ticket} -e -o {saveto}".format(
             rtmp=command, url=param.get('url'), content=content,
             ticket=param.get('ticket'), saveto=save_tmp)
-        logging.debug(com)
-        logging.info("[+] Starting get")
+        print("==> %s" % com)
         try:
             subprocess.check_call(shlex.split(com))
-            logging.info("[+] Complete get file: " + saveto)
+            print("Saved: " + saveto)
         except:
-            logging.warn("Failed to execute command: " + com)
+            logging.error("Failed to execute command: " + com)
 
     if len(contents) <= 1:
         # rename and move
         os.rename(save_tmp, saveto)
     else:
+        print("Join the files since this files are multiple")
         # combine files
         ffmpeg = which("ffmpeg")
 
@@ -64,18 +64,18 @@ def get(param, saveto, cmd="rtmpdump"):
         # ffmpeg -f concat -safe 0 -i input.txt -c copy <output.flv>
         com = '{ffmpeg} -f concat -safe 0 -i {input_txt} -c copy {saveto}'.format(
             ffmpeg=ffmpeg, input_txt=input_txt, saveto=saveto)
-        logging.debug(com)
+        print("==> %s" % com)
         try:
             subprocess.check_call(shlex.split(com))
-            logging.info("[+] Complete get file: " + saveto)
+            print("✓ Finihsed to join files: " + saveto)
         except:
-            logging.warn("Failed to execute command " + com)
+            logging.error("✗ Failed to execute command " + com)
 
 
 def convert(source_path, start_time, cutted_time, output_path):
     ffmpeg = which("ffmpeg")
     if not ffmpeg:
-        logging.critical("[+] rtmpdump not found. Please install rtmpdump on your PATH")
+        logging.critical("✗ rtmpdump not found. Please install rtmpdump on your PATH")
         sys.exit(1)
 
     # 1. cut file
@@ -86,29 +86,28 @@ def convert(source_path, start_time, cutted_time, output_path):
     com = "{ffmpeg} -ss {start_time} -i {source} -t {cutted_time} {temp_path}".format(
         ffmpeg=ffmpeg, start_time=start_time, source=source_path,
         cutted_time=cutted_time, temp_path=temp)
-    logging.debug(com)
+    print("==> %s" % com)
     try:
         subprocess.check_call(shlex.split(com))
-        logging.info("[+] Cutted")
     except:
-        logging.warn("Failed to execute command " + com)
+        logging.error("✗ Failed to execute command " + com)
 
     # 2. flv to mp3
     # ffmpeg -i <temp> -acodec libmp3lame -ab 256k <output_path>
     com = "{ffmpeg} -i {temp_path} -acodec libmp3lame -ab 256k {output_path}".format(
         ffmpeg=ffmpeg, temp_path=temp, output_path=output_path)
-    logging.debug(com)
+    print("==> %s" % com)
     try:
         subprocess.check_call(shlex.split(com))
-        logging.info("[+] Converted: " + output_path)
+        print("✓ Converted: " + output_path)
     except:
-        logging.warn("Failed to execute command " + com)
+        logging.error("✗ Failed to execute command " + com)
 
 
 def login():
     n = nico.Niconico()
     n.login()
-    logging.info("[+] Login success")
+    logging.debug("✓ Login success")
     return n
 
 
@@ -116,13 +115,13 @@ def get_wrapper(arg):
     arg = vars(arg)
     url_check = re.match(r'http:\/\/live.nicovideo.jp/watch/(?P<lv>lv[0-9]+)', arg.get('url'))
     if url_check is None:
-        sys.exit("Incorrect url patterns")
+        sys.exit("✗ Incorrect url patterns")
     lv = url_check.groupdict().get('lv')
     if lv:
         n = login()
         status = n.getplayerstatus(lv)
     else:
-        sys.exit("Incorrect url patterns")
+        sys.exit("✗ Incorrect url patterns")
     name = arg.get('save') if arg.get('save') else lv + ".flv"
     if status:
         get(status, name)
@@ -152,7 +151,7 @@ def parse():
                          required=True, type=str)
     convert.add_argument("-s", "--start", help="start time",
                          default=1800)
-    convert.add_argument("-t", help="cutting time", nargs=1, default=1770)
+    convert.add_argument("-t", help="cutting time", default=1770)
     convert.add_argument("-o", "--output", help="output filepath and filename",
                          type=str)
     convert.set_defaults(func=convert_wrapper)
@@ -170,13 +169,12 @@ def main():
     args = parse()
     arg = vars(args)
     if arg.get('debug') is True:
-        level = logging.NOTSET
+        level = logging.DEBUG
     elif arg.get('quiet') is True:
-        level = logging.CRITICAL
+        level = logging.ERROR
     else:
-        level = logging.INFO
-    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
-                        level=level)
+        level = logging.WARNING
+    logging.basicConfig(format='%(message)s', level=level)
     args.func(args)
 
 
